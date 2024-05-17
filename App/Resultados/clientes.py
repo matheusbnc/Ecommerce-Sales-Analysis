78ba57ba26fc828c881adf8670b9b433cf099eb1
matheusbnc <matheuschagasbn@gmail.com>
2024-05-17 12:39:34 -0300

    Adicionando funcoes da analise de clientes

--- App/Resultados/clientes.py
+++ App/Resultados/clientes.py
@@ -26,7 +26,33 @@ def more_units_sold_clients(sales_cli):
     
     print(sales_cli.head(15))
 
+def more_profitable_clients(sales_cli):
+    sleep(0.5)
+    
+    sales_cli = sales_cli.groupby('CustomerID').agg({'FinalPrice': 'sum'}).reset_index()
+    
+    sales_cli = sales_cli.sort_values(by=['FinalPrice'], ascending=False).reset_index(drop=True)
+    
+    plot_bar(sales_cli, x_axes='FinalPrice', y_axes='CustomerID', x_label='Valor Total (£)'\
+             ,y_label='Cliente', title='Clientes que Geraram Mais Receita'\
+             ,palette='crest')
+    
+    print(sales_cli.head(15))    
+    
 
+def frequent_clients(sales_cli):
+    sleep(0.5)
+    
+    sales_cli = sales_cli[sales_cli['Quantity'] > 0].groupby('CustomerID')\
+                                                .agg({'InvoiceNo': 'count'}).reset_index()
+    
+    sales_cli = sales_cli.sort_values(by=['InvoiceNo'], ascending=False).reset_index(drop=True)
+    
+    plot_bar(sales_cli, x_axes='InvoiceNo', y_axes='CustomerID', x_label='Ocorrências'\
+             ,y_label='Cliente', title='Clientes com Mais Ocorrências de Compra'\
+             ,palette='crest')
+    
+    print(sales_cli.head(15))  
 # In[ ]:
 
 
