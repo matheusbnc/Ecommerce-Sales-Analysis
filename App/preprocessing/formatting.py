d1a7051b7f14a7249ec26ffc1790135212ef06f9
matheusbnc <matheuschagasbn@gmail.com>
2024-05-21 10:21:10 -0300

    documentacao pacote formatting

--- App/preprocessing/formatting.py
+++ App/preprocessing/formatting.py
@@ -3,73 +3,84 @@ import numpy as np
 
 
 def not_number(value):
+    # Verifica se o valor não começa com um dígito
     return not value[0].isdigit()
 
 
 def title_col(data, col_name='Description'):
-    '''
-    Converte a primeira letra de cada palavra para maíuscula em uma coluna específica de um dataframe
-    
-    Parâmetros:
-    -----------    
-    data : pandas.DataFrame
-        O DataFrame onde a coluna está localizada
-    col_name : str
-        A coluna que será transformada. Deve ser string.
-    '''
+    # Converte a primeira letra de cada palavra para maiúscula em uma coluna específica de um DataFrame
     data[col_name] = data[col_name].apply(lambda x: x.title())
     return data
 
 
 def pre_processing(data):
-    
-    # Filtra todos os registros que não vendas/cancelamento de produtos
+    # Filtra todos os registros que não são vendas/cancelamento de produtos
     not_product_lines = data[data['StockCode'].apply(not_number)]
 
-    # Obter os valores únicos das linhas
+    # Remove do DataFrame os registros que não correspondem a vendas/cancelamentos de produtos
     not_product = not_product_lines['StockCode'].unique()
-    
     data = data[~data['StockCode'].isin(not_product)]
     
-    # Adicionando coluna de Preço Final de venda/devolução para cada transação
+    # Adiciona coluna de Preço Final de venda/devolução para cada transação
     data['FinalPrice'] = data['Quantity'] * data['UnitPrice']
     
-    # Excluindo os registros com valor unitário igual a zero
+    # Exclui os registros com valor unitário igual a zero
     data = data[data['UnitPrice'] > 0]
     
-    # Remove as linhas duplicadas 
+    # Remove as linhas duplicadas
     data = data.drop_duplicates()
     
-    # Converte a primeira letra de cada palavra para maíuscula em uma coluna específica de um dataframe
+    # Converte a primeira letra de cada palavra para maiúscula em uma coluna específica de um DataFrame
     title_col(data)
     
     return data
 
 
 def change_types(data):
-    data['InvoiceDate'] = pd.to_datetime(data['InvoiceDate'], format = '%d/%m/%Y %H:%M')
+    # Ajusta os tipos de dados de algumas colunas em um DataFrame
+    
+    # Converte a coluna InvoiceDate para datetime com formato '%d/%m/%Y %H:%M'
+    data['InvoiceDate'] = pd.to_datetime(data['InvoiceDate'], format='%d/%m/%Y %H:%M')
+    
+    # Converte a coluna CustomerID para string e remove qualquer ponto decimal
     data['CustomerID'] = data['CustomerID'].astype('str')
     data['CustomerID'] = data['CustomerID'].apply(lambda x: x.split('.')[0])
+    
+    # Converte a coluna Country para string
     data['Country'] = data['Country'].astype('str')
+    
     return data
 
 
 def drop_uk(data):
+    # Remove todas as linhas onde o valor da coluna Country é 'United Kingdom'
     data = data.drop(data[data['Country'] == 'United Kingdom'].index)
     return data
 
 
 def drop_nan(data, col_name='CustomerID'):
-    data = data.drop(data[data['CustomerID'] == 'nan'].index)
+    # Remove todas as linhas onde o valor da coluna especificada é 'nan'
+    data = data.drop(data[data[col_name] == 'nan'].index)
     return data
 
 
 def add_time_columns(data):
+    # Adiciona colunas relacionadas ao tempo baseadas na coluna InvoiceDate
+    
+    # Adiciona coluna com o nome do mês em português
     data['Month'] = data['InvoiceDate'].dt.month_name(locale='pt_BR')
+    
+    # Adiciona coluna com o nome do dia da semana em português
     data['Day'] = data['InvoiceDate'].dt.day_name(locale='pt_BR')
+    
+    # Adiciona coluna com o trimestre no formato 'T{n}'
     data['Quarter'] = data['InvoiceDate'].dt.quarter.apply(lambda x: f'T{x}')
+    
+    # Adiciona coluna com o ano
     data['Year'] = data['InvoiceDate'].dt.year
+    
     return data
+
 
     
 
